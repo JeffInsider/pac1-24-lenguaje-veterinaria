ea880fe3a63b14d522199fa66ddfe03a6bfc88f3
JeffInsider <jeffreyxavier15@gmail.com>
2024-02-14 18:41:27 -0600

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
     return(
         <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
             Seguimiento de mascotas {''}
-            <span className="text-indigo-600">Veterinaria</span>
+            <span className="text-indigo-600">Veterinaria /  Integracion Continua</span>
         </h1>
     )
 }
